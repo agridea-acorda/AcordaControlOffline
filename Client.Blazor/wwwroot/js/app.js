fd8ab9073f74885fb0ee4221cce5ba93a8cf786c
Nicolas Destrez <nicolas.destrez@agridea.ch>
2020-10-14 18:33:22 +0200

    Init adminlte's PushMenu and CardWidget using jquery api and blazor js interop. The data-api does not work out of the box because js code needs to be called explicitly.

---
+++ Client.Blazor/wwwroot/js/app.js
@@ -0,0 +1,6 @@
+﻿function initAdminlte() {
+    console.log('initializing adminLTE...');
+    $('[data-widget="pushmenu"]').PushMenu();
+    $('[data-card-widget="collapse"]').CardWidget('toggle');
+    console.log('Done.');
+}
