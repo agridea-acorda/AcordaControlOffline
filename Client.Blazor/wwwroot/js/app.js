bac7a25aea53ccb6b577098f18ff5678342e91a1
Nicolas Destrez <nicolas.destrez@agridea.ch>
2020-10-15 15:35:05 +0200

    add popper and bootstrap js + force init in 'initAdminlte' (in lieu of data-api not working out of the box with blazor wasm) + add markup for user dropdown

--- Client.Blazor/wwwroot/js/app.js
+++ Client.Blazor/wwwroot/js/app.js
@@ -2,5 +2,6 @@
     console.log('initializing adminLTE...');
     $('[data-widget="pushmenu"]').PushMenu();
     $('[data-card-widget="collapse"]').CardWidget('toggle');
+    $('.data-toggle').dropdown();
     console.log('Done.');
 }
