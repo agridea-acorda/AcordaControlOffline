e898923a2d8898557c3a3c5d1764481b14ac6e59
Nicolas Destrez <nicolas.destrez@agridea.ch>
2020-10-16 19:24:23 +0200

    in progress display list of mandates from data. Fail to read sample data from json file because of stupid file path issue......

---
+++ Shared.ApplicationServices/DTOs/Mandate.cs
@@ -0,0 +1,25 @@
+﻿namespace Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.DTOs
+{
+    public class Mandate
+    {
+        public Farm Farm { get; set; }
+        public Badge[] Badges { get; set; }
+    }
+
+    public class Farm
+    {
+        public string Ktidb { get; set; }
+        public string FarmName { get; set; }
+        public string Address { get; set; }
+        public string FarmType { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+
+    public class Badge
+    {
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+    }
+}
