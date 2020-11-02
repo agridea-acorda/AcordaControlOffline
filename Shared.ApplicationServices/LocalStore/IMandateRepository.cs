471e47f8c37b86c0632877b813a9268c8e14ad8f
Nicolas Destrez <nicolas.destrez@agridea.ch>
2020-11-02 18:25:07 +0100

    repository started. todo configure in blazor client and inject in pages/components in lieu of ILocalStorage.

---
+++ Shared.ApplicationServices/LocalStore/IMandateRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.ViewModel.MandateList;
+
+namespace Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.LocalStore {
+    public interface IMandateRepository
+    {
+        ValueTask SaveMandatesAsync(Mandate[] mandates);
+        ValueTask<Mandate[]> ReadAllMandatesAsync();
+        ValueTask<bool> HasMandateAsync(string key);
+        ValueTask<ViewModel.MandateDetail.Mandate> ReadMandateAsync(int farmId);
+    }
+}
---
+++ Shared.ApplicationServices/LocalStore/LocalStorageMandateRepository.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.ViewModel.MandateList;
+using Blazored.LocalStorage;
+
+namespace Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.LocalStore
+{
+    public class LocalStorageMandateRepository : IMandateRepository
+    {
+        public const string Mandates = "mandates";
+        public const string MandateDetail = "mandateDetail";
+
+        private readonly ILocalStorageService localStorage_;
+        public LocalStorageMandateRepository(ILocalStorageService localStorage)
+        {
+            localStorage_ = localStorage;
+        }
+
+        public async ValueTask SaveMandatesAsync(Mandate[] mandates)
+        {
+            await localStorage_.SetItemAsync(Mandates, mandates);
+        }
+
+        public async ValueTask<Mandate[]> ReadAllMandatesAsync()
+        {
+            return await localStorage_.GetItemAsync<Mandate[]>(Mandates);
+        }
+
+        public async ValueTask<bool> HasMandateAsync(string key)
+        {
+            return await localStorage_.ContainKeyAsync(key);
+        }
+
+        public async ValueTask<ViewModel.MandateDetail.Mandate> ReadMandateAsync(int farmId)
+        {
+            string key = MandateDetailKey(farmId);
+            return await localStorage_.GetItemAsync<ViewModel.MandateDetail.Mandate>(key);
+        }
+
+        private static string MandateDetailKey(int farmId)
+        {
+            return $"{MandateDetail}_{farmId}";
+        }
+    }
+}
