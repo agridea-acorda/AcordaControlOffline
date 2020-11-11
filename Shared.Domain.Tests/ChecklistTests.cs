e0fc4ea81eaf93a4ca2dc60d4c2744efee23ba49
Nicolas Destrez <nicolas.destrez@agridea.ch>
2020-11-11 15:46:30 +0100

    Checklist can serialize to json and deserialize to dto

--- Shared.Domain.Tests/ChecklistTests.cs
+++ Shared.Domain.Tests/ChecklistTests.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 using Agridea.Acorda.AcordaControlOffline.Shared.Domain.Checklist;
+using Agridea.DomainDrivenDesign;
 using FluentAssertions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Xunit;
 
 namespace Agridea.Acorda.AcordaControlOffline.Shared.Domain.Tests
 {
     public class ChecklistTests
     {
+        private Checklist.Checklist checklist_;
+
+        public ChecklistTests()
+        {
+            checklist_ = BuildChecklist();
+        }
+        
         private static Checklist.Checklist BuildChecklist()
         {
             var checklist = new Checklist.Checklist()
@@ -32,68 +44,147 @@ namespace Agridea.Acorda.AcordaControlOffline.Shared.Domain.Tests
             return checklist;
         }
 
+        
         [Fact]
         public void Checklist_tree_structure_should_be_consistent()
         {
-            var checklist = BuildChecklist();
-            
-            checklist.Rubrics.Count.Should().Be(2);
-            checklist.Rubrics["R1"].Children.Count.Should().Be(2);
-            checklist.Rubrics["R1"].Children.Should().ContainKeys("R1,P1", "R1,P2");
-            checklist.Rubrics["R2"].Children.Count.Should().Be(2);
-            checklist.Rubrics["R2"].Children.Should().ContainKeys("R2,G1", "R2,G2");
-
-            checklist.Rubrics["R1"].Children["R1,P1"].ConjunctElementCode.Should().Be("R1,P1");
-            checklist.Rubrics["R1"].Children["R1,P1"].ElementCode.Should().Be("P1");
-            checklist.Rubrics["R1"].Children["R1,P1"].ShortName.Should().Be("P1");
-            checklist.Rubrics["R1"].Children["R1,P1"].Parent.Should().NotBeNull();
-            checklist.Rubrics["R1"].Children["R1,P1"].Parent.ConjunctElementCode.Should().Be("R1");
-            checklist.Rubrics["R1"].Children["R1,P1"].Parent.ElementCode.Should().Be("R1");
-            checklist.Rubrics["R1"].Children["R1,P1"].Parent.ShortName.Should().Be("R1");
-            checklist.Rubrics["R1"].Children["R1,P2"].ConjunctElementCode.Should().Be("R1,P2");
-            checklist.Rubrics["R1"].Children["R1,P2"].ElementCode.Should().Be("P2");
-            checklist.Rubrics["R1"].Children["R1,P2"].ShortName.Should().Be("P2");
-            checklist.Rubrics["R1"].Children["R1,P2"].Parent.Should().NotBeNull();
-            checklist.Rubrics["R1"].Children["R1,P2"].Parent.ConjunctElementCode.Should().Be("R1");
-            checklist.Rubrics["R1"].Children["R1,P2"].Parent.ElementCode.Should().Be("R1");
-            checklist.Rubrics["R1"].Children["R1,P2"].Parent.ShortName.Should().Be("R1");
-
-            checklist.Rubrics["R2"].Children["R2,G1"].ConjunctElementCode.Should().Be("R2,G1");
-            checklist.Rubrics["R2"].Children["R2,G1"].ElementCode.Should().Be("G1");
-            checklist.Rubrics["R2"].Children["R2,G1"].ShortName.Should().Be("G1");
-            checklist.Rubrics["R2"].Children["R2,G1"].Parent.Should().NotBeNull();
-            checklist.Rubrics["R2"].Children["R2,G1"].Parent.ConjunctElementCode.Should().Be("R2");
-            checklist.Rubrics["R2"].Children["R2,G1"].Parent.ElementCode.Should().Be("R2");
-            checklist.Rubrics["R2"].Children["R2,G1"].Parent.ShortName.Should().Be("R2");
-            checklist.Rubrics["R2"].Children["R2,G1"].Children.Count.Should().Be(3);
-            checklist.Rubrics["R2"].Children["R2,G1"].Children.Should().ContainKeys("R2,G1,P1", "R2,G1,P2", "R2,G1,P3");
-            
-            checklist.Rubrics["R2"].Children["R2,G2"].ConjunctElementCode.Should().Be("R2,G2");
-            checklist.Rubrics["R2"].Children["R2,G2"].ElementCode.Should().Be("G2");
-            checklist.Rubrics["R2"].Children["R2,G2"].ShortName.Should().Be("G2");
-            checklist.Rubrics["R2"].Children["R2,G2"].Parent.Should().NotBeNull();
-            checklist.Rubrics["R2"].Children["R2,G2"].Parent.ConjunctElementCode.Should().Be("R2");
-            checklist.Rubrics["R2"].Children["R2,G2"].Parent.ElementCode.Should().Be("R2");
-            checklist.Rubrics["R2"].Children["R2,G2"].Parent.ShortName.Should().Be("R2");
-            checklist.Rubrics["R2"].Children["R2,G2"].Children.Count.Should().Be(2);
-            checklist.Rubrics["R2"].Children["R2,G2"].Children.Should().ContainKeys("R2,G2,SG1", "R2,G2,SG2");
-            
-            checklist.Rubrics["R2"].Children["R2,G2"].Children["R2,G2,SG1"].Children.Count.Should().Be(4);
-            checklist.Rubrics["R2"].Children["R2,G2"].Children["R2,G2,SG1"].Children.Should().ContainKeys("R2,G2,SG1,P1", "R2,G2,SG1,P2", "R2,G2,SG1,P3", "R2,G2,SG1,P4");
-            checklist.Rubrics["R2"].Children["R2,G2"].Children["R2,G2,SG2"].Children.Count.Should().Be(2);
-            checklist.Rubrics["R2"].Children["R2,G2"].Children["R2,G2,SG2"].Children.Should().ContainKeys("R2,G2,SG2,P1", "R2,G2,SG2,P2");
+            checklist_.Rubrics.Count.Should().Be(2);
+            checklist_.Rubrics["R1"].Children.Count.Should().Be(2);
+            checklist_.Rubrics["R1"].Children.Should().ContainKeys("R1,P1", "R1,P2");
+            checklist_.Rubrics["R2"].Children.Count.Should().Be(2);
+            checklist_.Rubrics["R2"].Children.Should().ContainKeys("R2,G1", "R2,G2");
+                     
+            checklist_.Rubrics["R1"].Children["R1,P1"].ConjunctElementCode.Should().Be("R1,P1");
+            checklist_.Rubrics["R1"].Children["R1,P1"].ElementCode.Should().Be("P1");
+            checklist_.Rubrics["R1"].Children["R1,P1"].ShortName.Should().Be("P1");
+            checklist_.Rubrics["R1"].Children["R1,P1"].Parent.Should().NotBeNull();
+            checklist_.Rubrics["R1"].Children["R1,P1"].Parent.ConjunctElementCode.Should().Be("R1");
+            checklist_.Rubrics["R1"].Children["R1,P1"].Parent.ElementCode.Should().Be("R1");
+            checklist_.Rubrics["R1"].Children["R1,P1"].Parent.ShortName.Should().Be("R1");
+            checklist_.Rubrics["R1"].Children["R1,P2"].ConjunctElementCode.Should().Be("R1,P2");
+            checklist_.Rubrics["R1"].Children["R1,P2"].ElementCode.Should().Be("P2");
+            checklist_.Rubrics["R1"].Children["R1,P2"].ShortName.Should().Be("P2");
+            checklist_.Rubrics["R1"].Children["R1,P2"].Parent.Should().NotBeNull();
+            checklist_.Rubrics["R1"].Children["R1,P2"].Parent.ConjunctElementCode.Should().Be("R1");
+            checklist_.Rubrics["R1"].Children["R1,P2"].Parent.ElementCode.Should().Be("R1");
+            checklist_.Rubrics["R1"].Children["R1,P2"].Parent.ShortName.Should().Be("R1");
+                     
+            checklist_.Rubrics["R2"].Children["R2,G1"].ConjunctElementCode.Should().Be("R2,G1");
+            checklist_.Rubrics["R2"].Children["R2,G1"].ElementCode.Should().Be("G1");
+            checklist_.Rubrics["R2"].Children["R2,G1"].ShortName.Should().Be("G1");
+            checklist_.Rubrics["R2"].Children["R2,G1"].Parent.Should().NotBeNull();
+            checklist_.Rubrics["R2"].Children["R2,G1"].Parent.ConjunctElementCode.Should().Be("R2");
+            checklist_.Rubrics["R2"].Children["R2,G1"].Parent.ElementCode.Should().Be("R2");
+            checklist_.Rubrics["R2"].Children["R2,G1"].Parent.ShortName.Should().Be("R2");
+            checklist_.Rubrics["R2"].Children["R2,G1"].Children.Count.Should().Be(3);
+            checklist_.Rubrics["R2"].Children["R2,G1"].Children.Should().ContainKeys("R2,G1,P1", "R2,G1,P2", "R2,G1,P3");
+                     
+            checklist_.Rubrics["R2"].Children["R2,G2"].ConjunctElementCode.Should().Be("R2,G2");
+            checklist_.Rubrics["R2"].Children["R2,G2"].ElementCode.Should().Be("G2");
+            checklist_.Rubrics["R2"].Children["R2,G2"].ShortName.Should().Be("G2");
+            checklist_.Rubrics["R2"].Children["R2,G2"].Parent.Should().NotBeNull();
+            checklist_.Rubrics["R2"].Children["R2,G2"].Parent.ConjunctElementCode.Should().Be("R2");
+            checklist_.Rubrics["R2"].Children["R2,G2"].Parent.ElementCode.Should().Be("R2");
+            checklist_.Rubrics["R2"].Children["R2,G2"].Parent.ShortName.Should().Be("R2");
+            checklist_.Rubrics["R2"].Children["R2,G2"].Children.Count.Should().Be(2);
+            checklist_.Rubrics["R2"].Children["R2,G2"].Children.Should().ContainKeys("R2,G2,SG1", "R2,G2,SG2");
+                     
+            checklist_.Rubrics["R2"].Children["R2,G2"].Children["R2,G2,SG1"].Children.Count.Should().Be(4);
+            checklist_.Rubrics["R2"].Children["R2,G2"].Children["R2,G2,SG1"].Children.Should().ContainKeys("R2,G2,SG1,P1", "R2,G2,SG1,P2", "R2,G2,SG1,P3", "R2,G2,SG1,P4");
+            checklist_.Rubrics["R2"].Children["R2,G2"].Children["R2,G2,SG2"].Children.Count.Should().Be(2);
+            checklist_.Rubrics["R2"].Children["R2,G2"].Children["R2,G2,SG2"].Children.Should().ContainKeys("R2,G2,SG2,P1", "R2,G2,SG2,P2");
 
         }
 
         [Fact]
         public void Can_find_nodes_in_checklist()
         {
-            var checklist = BuildChecklist();
+            checklist_.Rubrics["R2"].Find("R2").Should().NotBeNull();
+            checklist_.Rubrics["R2"].Find("R2").ConjunctElementCode.Should().Be("R2");
+            checklist_.Rubrics["R2"].Find("R2,G2").Should().NotBeNull();
+            checklist_.Rubrics["R2"].Find("R2,G2").ConjunctElementCode.Should().Be("R2,G2");
+        }
 
-            checklist.Rubrics["R2"].Find("R2").Should().NotBeNull();
-            checklist.Rubrics["R2"].Find("R2").ConjunctElementCode.Should().Be("R2");
-            checklist.Rubrics["R2"].Find("R2,G2").Should().NotBeNull();
-            checklist.Rubrics["R2"].Find("R2,G2").ConjunctElementCode.Should().Be("R2,G2");
+        [Fact]
+        public void Can_serialize()
+        {
+            string json = JsonConvert.SerializeObject(checklist_, 
+                                                      Formatting.Indented,
+                                                      new JsonSerializerSettings
+                                                      {
+                                                          ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                                                          ContractResolver = new ChecklistContractResolver()
+                                                      });
+            json.Should().NotBeEmpty();
+            File.WriteAllText(Path.Combine(Path.GetTempPath(), "checklist.json"), json);
+        }
+
+        [Fact]
+        public void Can_deserialize()
+        {
+            var json = File.ReadAllText("./Data/checklist.json");
+            var dto = JsonConvert.DeserializeObject<ChecklistDeserializationDto>(json);
+            dto.Should().NotBeNull();
+            
+            dto.Rubrics.Should().NotBeEmpty();
+            dto.Rubrics.Count.Should().Be(2);
+            dto.Rubrics["R1"].Should().NotBeNull();
+            dto.Rubrics["R2"].Should().NotBeNull();
+            dto.Rubrics["R1"].Children.Count.Should().Be(2);
+            dto.Rubrics["R1"].Children.Should().ContainKeys("R1,P1", "R1,P2");
+            dto.Rubrics["R2"].Children.Count.Should().Be(2);
+            dto.Rubrics["R2"].Children.Should().ContainKeys("R2,G1", "R2,G2");
+
+            dto.Rubrics["R1"].Children["R1,P1"].ConjunctElementCode.Should().Be("R1,P1");
+            dto.Rubrics["R1"].Children["R1,P1"].ElementCode.Should().Be("P1");
+            dto.Rubrics["R1"].Children["R1,P1"].ShortName.Should().Be("P1");
+            dto.Rubrics["R1"].Children["R1,P2"].ConjunctElementCode.Should().Be("R1,P2");
+            dto.Rubrics["R1"].Children["R1,P2"].ElementCode.Should().Be("P2");
+            dto.Rubrics["R1"].Children["R1,P2"].ShortName.Should().Be("P2");
+            
+            dto.Rubrics["R2"].Children["R2,G1"].ConjunctElementCode.Should().Be("R2,G1");
+            dto.Rubrics["R2"].Children["R2,G1"].ElementCode.Should().Be("G1");
+            dto.Rubrics["R2"].Children["R2,G1"].ShortName.Should().Be("G1");
+            dto.Rubrics["R2"].Children["R2,G1"].Children.Count.Should().Be(3);
+            dto.Rubrics["R2"].Children["R2,G1"].Children.Should().ContainKeys("R2,G1,P1", "R2,G1,P2", "R2,G1,P3");
+
+            dto.Rubrics["R2"].Children["R2,G2"].ConjunctElementCode.Should().Be("R2,G2");
+            dto.Rubrics["R2"].Children["R2,G2"].ElementCode.Should().Be("G2");
+            dto.Rubrics["R2"].Children["R2,G2"].ShortName.Should().Be("G2");
+            dto.Rubrics["R2"].Children["R2,G2"].Children.Count.Should().Be(2);
+            dto.Rubrics["R2"].Children["R2,G2"].Children.Should().ContainKeys("R2,G2,SG1", "R2,G2,SG2");
+            
+            dto.Rubrics["R2"].Children["R2,G2"].Children["R2,G2,SG1"].Children.Count.Should().Be(4);
+            dto.Rubrics["R2"].Children["R2,G2"].Children["R2,G2,SG1"].Children.Should().ContainKeys("R2,G2,SG1,P1", "R2,G2,SG1,P2", "R2,G2,SG1,P3", "R2,G2,SG1,P4");
+            dto.Rubrics["R2"].Children["R2,G2"].Children["R2,G2,SG2"].Children.Count.Should().Be(2);
+            dto.Rubrics["R2"].Children["R2,G2"].Children["R2,G2,SG2"].Children.Should().ContainKeys("R2,G2,SG2,P1", "R2,G2,SG2,P2");
+        }
+    }
+
+    public class EntityContractResolver : DefaultContractResolver
+    {
+        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+        {
+            IList<JsonProperty> props = base.CreateProperties(type, memberSerialization);
+            return props.Where(p =>
+                                   p.PropertyName != nameof(Entity.CreatedBy) &&
+                                   p.PropertyName != nameof(Entity.CreationDate) &&
+                                   p.PropertyName != nameof(Entity.ModifiedBy) &&
+                                   p.PropertyName != nameof(Entity.ModificationDate)
+                        )
+                        .ToList();
+        }
+    }
+
+    public class ChecklistContractResolver : EntityContractResolver
+    {
+        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+        {
+            IList<JsonProperty> props = base.CreateProperties(type, memberSerialization);
+            return props.Where(p =>
+                                   p.PropertyName != nameof(Checklist.Checklist.DomainEvents)
+                        )
+                        .ToList();
         }
     }
 }
---
+++ Shared.Domain/Checklist/Serialization/ChecklistDeserializationDto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agridea.Acorda.AcordaControlOffline.Shared.Domain.Tests
+{
+    public class ChecklistDeserializationDto
+    {
+        public SortedList<string, Result> Rubrics { get; set; }
+
+        public class Result
+        {
+            public Result Parent { get; set; }
+            public SortedList<string, Result> Children { get; set; }
+
+            public string ConjunctElementCode { get; set; }
+            public string Name { get; set; }
+            public string ElementCode { get; set; }
+            public string ShortName { get; set; }
+            public InspectionOutcome Outcome { get; set; }
+            public string InspectorComment { get; set; }
+            public string FarmerComment { get; set; }
+            public Defect Defect { get; set; }
+            public DefectSeriousness Seriousness { get; set; }
+            public double Percent { get; set; }
+        }
+
+        public class InspectionOutcome
+        {
+
+            public string Text { get; set; }
+            public int Value { get; set; }
+            public string Code { get; set; }
+
+        }
+
+        public class DefectSeriousness
+        {
+
+            public int Code { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class Defect
+        {
+            public string Description { get; set; }
+            public Measurement Size { get; set; }
+
+
+            public class Measurement
+            {
+                public double Size { get; set; }
+                public string Unit { get; set; }
+
+            }
+        }
+    }
+}
