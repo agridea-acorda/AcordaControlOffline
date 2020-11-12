a603a460a9054bc3d998dfea749aaff93ea8d31c
Nicolas Destrez <nicolas.destrez@agridea.ch>
2020-11-12 08:52:44 +0100

    Try to use tooltip on 'upload' button, but does not work

--- Client.Blazor/wwwroot/js/app.js
+++ Client.Blazor/wwwroot/js/app.js
@@ -3,5 +3,6 @@
     $('[data-widget="pushmenu"]').PushMenu();
     $('[data-card-widget="collapse"]').CardWidget('toggle');
     $('.data-toggle').dropdown();
+    $('[data-toggle="tooltip"]').tooltip();
     console.log('Done.');
 }
