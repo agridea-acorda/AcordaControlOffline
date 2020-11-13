8ae2db7cabb823c6072ac0f80a60c86b63354258
Nicolas Destrez <nicolas.destrez@agridea.ch>
2020-11-13 15:02:59 +0100

    Use Config.BaseUrl to fix navigation with gh-pages. Sucks but could not get it to work otherwise.

---
+++ Client.Blazor/UiServices/Config.cs
@@ -0,0 +1,11 @@
+﻿namespace Agridea.Acorda.AcordaControlOffline.Client.Blazor.UiServices
+{
+    public class Config
+    {
+#if DEBUG
+        public const string BaseUrl = "";
+#else
+        public const string BaseUrl = "/AcordaControlOffline";
+#endif
+    }
+}
