6dfcbe17117f8de44e66722537b0848acaebbecc
Nicolas Destrez <nicolas.destrez@agridea.ch>
2020-11-13 13:56:18 +0100

    Add brotli decompression as per ms doc https://docs.microsoft.com/en-us/aspnet/core/blazor/host-and-deploy/webassembly?view=aspnetcore-3.1

--- Client.Blazor/Program.cs
+++ Client.Blazor/Program.cs
@@ -1,11 +1,8 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices;
 using Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.Api;
 using Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.LocalStore;
-using Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.Queries;
-using Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.ViewModel.MandateList;
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,7 +15,11 @@ namespace Agridea.Acorda.AcordaControlOffline.Client.Blazor
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
+            
+            // Below is just a temporary hack for the /checklist page, still in demo state.
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            
+            // This is the real api
             builder.Services.AddHttpClient<IApiClient, SampleDataApiClient>(nameof(SampleDataApiClient),
                                                                           client =>
                                                                           {
@@ -26,11 +27,16 @@ namespace Agridea.Acorda.AcordaControlOffline.Client.Blazor
                                                                               // for real api: client.BaseAddress = new Uri(apiSettings.BaseAddress);
                                                                               // for real api: client.DefaultRequestHeaders.Add("api-key", apiSettings.ApiKey);
                                                                           });
+            
+            // local storage and repository using it
             builder.Services.AddBlazoredLocalStorage(config => config.JsonSerializerOptions.WriteIndented = true);
             builder.Services.AddScoped<IRepository, LocalStorageRepository>();
+            
+            // Attempts at CQRS commands and queries handlers but cannot get it to work
             //builder.Services.AddHandlers();
             //builder.Services.AddTransient<IQueryHandler<MandateListQuery, ValueTask<Mandate[]>>, MandateListQuery.MandateListQueryHandler>(x => new MandateListQuery.MandateListQueryHandler(x.GetService<IRepository>()));
             //builder.Services.AddSingleton<Messages>();
+            
             await builder.Build().RunAsync();
         }
     }
