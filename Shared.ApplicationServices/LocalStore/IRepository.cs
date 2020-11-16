86545f42a0b942ec9765e2ef841dc77a9d69113d
Nicolas Destrez <nicolas.destrez@agridea.ch>
2020-11-16 15:38:17 +0100

    /Signature can save sample actions or document data, with change detection. Save button is shown only when model is changed. Changed event is triggered when focus is lost on any of the form inputs, not on keypress.

--- Shared.ApplicationServices/LocalStore/IRepository.cs
+++ Shared.ApplicationServices/LocalStore/IRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.ViewModel.Checklist;
 using Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.ViewModel.MandateList;
+using Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.ViewModel.Signature;
 
 namespace Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.LocalStore {
     public interface IRepository
@@ -12,5 +13,7 @@ namespace Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.LocalSt
         ValueTask SaveMandateAsync(ViewModel.MandateDetail.Mandate mandate, int id = 0);
         ValueTask<ChecklistSample> ReadChecklistSampleAsync();
         ValueTask SaveChecklistSampleAsync(ChecklistSample checklist);
+        ValueTask<ActionsOrDocumentEditModel> ReadActionsOrDocumentsAsync();
+        ValueTask SaveActionsOrDocumentsAsync(ActionsOrDocumentEditModel model);
     }
 }
