9a5ab234154f8c1ceafe4d0ab4f3195e4ae774fd
Nicolas Destrez <nicolas.destrez@agridea.ch>
2020-12-02 12:16:37 +0100

    remove Mandate, keep only 3 aggregate roots: Farm, Inspection, Checklist

--- Shared.ApplicationServices/LocalStore/Serialization/Checklist/ChecklistFactory.cs
+++ Shared.ApplicationServices/LocalStore/Serialization/Checklist/ChecklistFactory.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.Serialization;
-using Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.LocalStore.Serialization.Mandate;
 using Agridea.Acorda.AcordaControlOffline.Shared.Domain.Checklist;
 using Newtonsoft.Json;
 
