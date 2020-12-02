38de0fc55a8fe51a600d0a0b2a49e4035c9f1c3a
Nicolas Destrez <nicolas.destrez@agridea.ch>
2020-12-02 12:33:40 +0100

    scaffold FarmFactory and InspectionFactory

---
+++ Shared.ApplicationServices/LocalStore/Serialization/Farm/FarmFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.LocalStore.Serialization.Farm
+{
+    public class FarmFactory : AggregateRootFactoryBase<Domain.Farm.Farm>
+    {
+        public override Domain.Farm.Farm Parse(string json)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Serialize(Domain.Farm.Farm aggregateRoot)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Shared.ApplicationServices/LocalStore/Serialization/Inspection/InspectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.LocalStore.Serialization.Inspection
+{
+    public class InspectionFactory : AggregateRootFactoryBase<Domain.Inspection.Inspection>
+    {
+        public override Domain.Inspection.Inspection Parse(string json)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Serialize(Domain.Inspection.Inspection aggregateRoot)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
