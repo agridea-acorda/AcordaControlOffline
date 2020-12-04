5de30392a5e95b9f5b0c54d181438f9833adc327
Nicolas Destrez <nicolas.destrez@agridea.ch>
2020-12-04 15:00:06 +0100

    new sample data file for MandateList with real data from api. Adjust bits and pieces in ui,

--- Client.Blazor/Program.cs
+++ Client.Blazor/Program.cs
@@ -25,9 +25,12 @@ namespace Agridea.Acorda.AcordaControlOffline.Client.Blazor
             builder.Services.AddHttpClient<IApiClient, SampleDataApiClient>(nameof(SampleDataApiClient),
                                                                           client =>
                                                                           {
+                                                                              // sample data in local json file
                                                                               client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
-                                                                              // for real api: client.BaseAddress = new Uri(apiSettings.BaseAddress);
-                                                                              // for real api: client.DefaultRequestHeaders.Add("api-key", apiSettings.ApiKey);
+                                                                              
+                                                                              //// real api
+                                                                              //client.BaseAddress = new Uri(apiSettings.BaseAddress);
+                                                                              //client.DefaultRequestHeaders.Add("api-key", apiSettings.ApiKey);
                                                                           });
             
             // local storage and repository using it
