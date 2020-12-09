eac32d75d78368a3c921a05a849eed2f486a252a
Nicolas Destrez <nicolas.destrez@agridea.ch>
2020-12-09 18:32:23 +0100

    Checklist.razor: using AR directly in page. Need to fix repository read/write and get rid of dummy sample viewmodel data.

--- Shared.Domain/Inspection/InspectionOutcome.cs
+++ Shared.Domain/Inspection/InspectionOutcome.cs
@@ -16,6 +16,7 @@ namespace Agridea.Acorda.AcordaControlOffline.Shared.Domain.Inspection
         public static InspectionOutcome NotOk => new InspectionOutcome(2, "NOk", Resources.InspectionOutcomeNotOk);
         public static InspectionOutcome NotInspected => new InspectionOutcome(3, "NotInspected", Resources.InspectionOutcomeNotInspected);
         public static InspectionOutcome NotApplicable => new InspectionOutcome(4, "NotApplicable", Resources.InspectionOutcomeNotApplicable);
+        public static InspectionOutcome Unset => new InspectionOutcome(-1, "Unset", "Inconnu");
 
         public string Text { get; }
         public int Value { get; }
