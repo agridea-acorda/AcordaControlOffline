594d8c651f57c7f7bc874b7c615a5eba00b3e99d
Nicolas Destrez <nicolas.destrez@agridea.ch>
2020-12-11 15:36:03 +0100

    Checklist can remember collapsed/expanded + can collapse/expand all

--- Client.Blazor/UiServices/MIscExtensions.cs
+++ Client.Blazor/UiServices/MIscExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Agridea.Acorda.AcordaControlOffline.Client.Blazor.UiServices
 {
@@ -7,6 +9,11 @@ namespace Agridea.Acorda.AcordaControlOffline.Client.Blazor.UiServices
         public static string ToDetailedTime(this DateTime dt)
         {
             return $"{dt.ToLongTimeString()} {dt.Millisecond:000}ms";
+        }
+
+        public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> source)
+        {
+            return source.Select((item, index) => (item, index));
         }
     }
 }
