68e5edac18af3335bec2b16e090c7f8e7133aa2a
Nicolas Destrez <nicolas.destrez@agridea.ch>
2020-12-28 19:03:04 +0100

    PageBase to inherit from. All pages should inherit from this. Did it with Checklist2. => Then check authentication from base page.

---
+++ Client.Blazor/PageBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace Agridea.Acorda.AcordaControlOffline.Client.Blazor
+{
+    public class PageBase : ComponentBase
+    {
+        protected override void OnInitialized()
+        {
+            Console.WriteLine("Hello from PageBase");
+            base.OnInitialized();
+        }
+    }
+}
