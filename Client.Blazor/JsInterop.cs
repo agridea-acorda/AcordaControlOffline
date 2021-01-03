d1034e1bd7968ebe0e427f9cfb0fd646b21c712c
Nicolas Destrez <nicolas.destrez@hotmail.com>
2021-01-03 18:47:32 +0100

    new Home page to redirect to Login or MandateList + convert MandateList to page + new UserDisplay and CantonDisplay components. Home page checks if logged-in via cookie (validity one week ? one month ? one year ?)

---
+++ Client.Blazor/JsInterop.cs
@@ -0,0 +1,10 @@
+﻿namespace Agridea.Acorda.AcordaControlOffline.Client.Blazor
+{
+    public class JsInterop
+    {
+        public const string ReadCookie = "blazorExtensions.ReadCookie";
+        public const string SetCookie = "blazorExtensions.SetCookie";
+        public const string RemoveCookie = "blazorExtensions.RemoveCookie";
+        public const string InitAdminLte = "initAdminlte";
+    }
+}
