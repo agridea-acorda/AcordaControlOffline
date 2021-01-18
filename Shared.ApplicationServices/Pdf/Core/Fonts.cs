ae89c5325594863d4d66b0563ba4cfd1c9fbe342
Nicolas Destrez <nicolas.destrez@agridea.ch>
2021-01-18 09:56:52 +0100

    added core pdf classes

---
+++ Shared.ApplicationServices/Pdf/Core/Fonts.cs
@@ -0,0 +1,46 @@
+﻿using iTextSharp.text;
+
+namespace Agridea.Pdf
+{
+    public class Fonts
+    {
+        public static readonly Font Helvetica6Black = new Font(Font.FontFamily.HELVETICA, 6f, 0, Colors.Black);
+        public static readonly Font Helvetica8Black = new Font(Font.FontFamily.HELVETICA, 8f, 0, Colors.Black);
+        public static readonly Font Helvetica8BlackItalic = new Font(Font.FontFamily.HELVETICA, 8f, Font.ITALIC, Colors.Black);
+        //public static readonly Font Helvetica8Black = GetHelvetica8Black();
+        public static readonly Font Helvetica9Black = new Font(Font.FontFamily.HELVETICA, 9f, 0, Colors.Black);
+        public static readonly Font Helvetica10Black = new Font(Font.FontFamily.HELVETICA, 10f, 0, Colors.Black);
+        public static readonly Font Helvetica11Black = new Font(Font.FontFamily.HELVETICA, 11f, 0, Colors.Black);
+        public static readonly Font Helvetica12Black = new Font(Font.FontFamily.HELVETICA, 12f, 0, Colors.Black);
+        public static readonly Font Helvetica14Black = new Font(Font.FontFamily.HELVETICA, 14f, 0, Colors.Black);
+        public static readonly Font Helvetica16Black = new Font(Font.FontFamily.HELVETICA, 16f, 0, Colors.Black);
+        public static readonly Font Helvetica18Black = new Font(Font.FontFamily.HELVETICA, 18f, 0, Colors.Black);
+        public static readonly Font Helvetica20Black = new Font(Font.FontFamily.HELVETICA, 20f, 0, Colors.Black);
+        public static readonly Font Helvetica22Black = new Font(Font.FontFamily.HELVETICA, 22f, 0, Colors.Black);
+        public static readonly Font Helvetica24Black = new Font(Font.FontFamily.HELVETICA, 24f, 0, Colors.Black);
+        public static readonly Font Helvetica26Black = new Font(Font.FontFamily.HELVETICA, 26f, 0, Colors.Black);
+        public static readonly Font Helvetica28Black = new Font(Font.FontFamily.HELVETICA, 28f, 0, Colors.Black);
+        public static readonly Font Helvetica36Black = new Font(Font.FontFamily.HELVETICA, 36f, 0, Colors.Black);
+        public static readonly Font Helvetica48Black = new Font(Font.FontFamily.HELVETICA, 48f, 0, Colors.Black);
+        public static readonly Font Helvetica72Black = new Font(Font.FontFamily.HELVETICA, 72f, 0, Colors.Black);
+
+        public static readonly Font Helvetica6BlackBold = new Font(Font.FontFamily.HELVETICA, 6f, Font.BOLD, Colors.Black);
+        public static readonly Font Helvetica8BlackBold = new Font(Font.FontFamily.HELVETICA, 8f, Font.BOLD, Colors.Black);
+        public static readonly Font Helvetica8BlackBoldItalic = new Font(Font.FontFamily.HELVETICA, 8f, Font.BOLDITALIC, Colors.Black);
+        public static readonly Font Helvetica9BlackBold = new Font(Font.FontFamily.HELVETICA, 9f, Font.BOLD, Colors.Black);
+        public static readonly Font Helvetica10BlackBold = new Font(Font.FontFamily.HELVETICA, 10f, Font.BOLD, Colors.Black);
+        public static readonly Font Helvetica11BlackBold = new Font(Font.FontFamily.HELVETICA, 11f, Font.BOLD, Colors.Black);
+        public static readonly Font Helvetica12BlackBold = new Font(Font.FontFamily.HELVETICA, 12f, Font.BOLD, Colors.Black);
+        public static readonly Font Helvetica14BlackBold = new Font(Font.FontFamily.HELVETICA, 14f, Font.BOLD, Colors.Black);
+        public static readonly Font Helvetica16BlackBold = new Font(Font.FontFamily.HELVETICA, 16f, Font.BOLD, Colors.Black);
+        public static readonly Font Helvetica18BlackBold = new Font(Font.FontFamily.HELVETICA, 18f, Font.BOLD, Colors.Black);
+        public static readonly Font Helvetica20BlackBold = new Font(Font.FontFamily.HELVETICA, 20f, Font.BOLD, Colors.Black);
+        public static readonly Font Helvetica22BlackBold = new Font(Font.FontFamily.HELVETICA, 22f, Font.BOLD, Colors.Black);
+        public static readonly Font Helvetica24BlackBold = new Font(Font.FontFamily.HELVETICA, 24f, Font.BOLD, Colors.Black);
+        public static readonly Font Helvetica26BlackBold = new Font(Font.FontFamily.HELVETICA, 26f, Font.BOLD, Colors.Black);
+        public static readonly Font Helvetica28BlackBold = new Font(Font.FontFamily.HELVETICA, 28f, Font.BOLD, Colors.Black);
+        public static readonly Font Helvetica36BlackBold = new Font(Font.FontFamily.HELVETICA, 36f, Font.BOLD, Colors.Black);
+        public static readonly Font Helvetica48BlackBold = new Font(Font.FontFamily.HELVETICA, 48f, Font.BOLD, Colors.Black);
+        public static readonly Font Helvetica72BlackBold = new Font(Font.FontFamily.HELVETICA, 72f, Font.BOLD, Colors.Black);
+    }
+}
