4efdb64f5e68e5c7dcef30ba0adc11cdd58a5c6a
Moor Lucien <Lucien.Moor@agridea.ch>
2021-01-19 14:40:12 +0100

    download temporary controle PDF

---
+++ Shared.Domain/Pdf/Model/Rubric/IIndexedRenderer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agridea.Acorda.AcordaControlOffline.Shared.Domain.Pdf.Model.Rubric
+{
+    public interface IIndexedRenderer
+    {
+        #region Services
+
+        string Render(int index);
+
+        #endregion
+    }
+}
---
+++ Shared.Domain/Pdf/Shared/IPdfBodyDelegate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using iTextSharp.text;
+
+namespace Agridea.Acorda.AcordaControlOffline.Shared.Domain.Pdf.Shared
+{
+
+    /// <summary>
+    /// Fulfilled by a Pdf report maker to define the body of a Pdf document
+    /// </summary>
+    public interface IPdfBodyDelegate
+    {
+        void AddBody(Document document);
+    }
+    
+}
