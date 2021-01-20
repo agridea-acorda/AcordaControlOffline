f914bdd1068b74f22d79786dd1a844a1f23bfdee
Nicolas Destrez <nicolas.destrez@agridea.ch>
2021-01-20 09:18:42 +0100

    tentative autosave class

---
+++ Client.Blazor/UiServices/Autosave.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Xml.Linq;
+using MediatR;
+
+namespace Agridea.Acorda.AcordaControlOffline.Client.Blazor.UiServices
+{
+    public class Autosave : IDisposable
+    {
+        public int SaveIntervalInMsDefault = 3 * 1000; // three seconds
+        public int SavedMessageDisplayLengthInMsDefault = 5 * 1000; // five seconds
+        private Timer savingTimer_;
+        private Timer savedTimer_;
+        private SavingState state;
+        private Task saveAction_;
+
+        public SavingState State => state;
+
+        public Autosave(Task saveAction)
+        {
+            saveAction_ = saveAction;
+            savingTimer_ = new Timer(SaveIntervalInMsDefault) {AutoReset = false};
+            savingTimer_.Elapsed += async (sender, e) =>
+            {
+                state = SavingState.Saving;
+                await WaitForActionToComplete();
+            };
+            savedTimer_ = new Timer(SavedMessageDisplayLengthInMsDefault) {AutoReset = false};
+            savedTimer_.Elapsed += (sender, e) => state = SavingState.Idle;
+            state = SavingState.Idle;
+        }
+
+        private async Task WaitForActionToComplete()
+        {
+            await saveAction_;
+            savedTimer_.Stop();
+            savedTimer_.Start();
+            state = SavingState.Displaying;
+        }
+
+        public void Save()
+        {
+            savingTimer_.Stop();
+            savingTimer_.Start();
+        }
+
+        public void Dispose()
+        {
+            savingTimer_?.Dispose();
+            savedTimer_?.Dispose();
+        }
+
+        public enum SavingState
+        {
+            Idle,
+            Saving,
+            Displaying
+        }
+    }
+}
