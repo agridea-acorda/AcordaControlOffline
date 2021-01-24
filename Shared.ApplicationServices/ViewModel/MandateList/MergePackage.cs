82e37b382fbb11a1d7f122f0726b10bb47126342
Nicolas Destrez <nicolas.destrez@agridea.ch>
2021-01-24 19:09:18 +0100

    Merge package to send to server

---
+++ Shared.ApplicationServices/ViewModel/MandateList/MergePackage.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.LocalStore.Serialization.Checklist;
+using Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.LocalStore.Serialization.Mandate;
+using EnsureThat;
+
+namespace Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.ViewModel.MandateList
+{
+    public class MergePackage
+    {
+        public string Mandate { get; set; }
+        public string[] Checklists { get; set; }
+
+        public static MergePackage FromDomain(Domain.Mandate.Mandate mandate, Domain.Checklist.Checklist[] checklists)
+        {
+            Ensure.That(mandate, nameof(mandate)).IsNotNull();
+            Ensure.That(checklists, nameof(checklists)).IsNotNull();
+            Ensure.That(checklists, nameof(checklists)).HasItems();
+            var mergePackage = new MergePackage();
+            var mandateFactory = new MandateFactory();
+            var checklistFactory = new ChecklistFactory();
+            mergePackage.Mandate = mandateFactory.Serialize(mandate);
+            mergePackage.Checklists = checklists.Select(checklistFactory.Serialize).ToArray();
+            return mergePackage;
+        }
+    }
+}
