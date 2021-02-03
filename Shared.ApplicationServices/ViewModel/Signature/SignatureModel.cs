3753f9d291cb20c03a543a9495877bac5a9ceca2
Nicolas Destrez <nicolas.destrez@agridea.ch>
2021-02-03 15:54:26 +0100

    Signature set FinishStatus if type=inspector

--- Shared.ApplicationServices/ViewModel/Signature/SignatureModel.cs
+++ Shared.ApplicationServices/ViewModel/Signature/SignatureModel.cs
@@ -1,4 +1,7 @@
-﻿namespace Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.ViewModel.Signature
+﻿using System;
+using Agridea.Acorda.AcordaControlOffline.Shared.Domain.Inspection;
+
+namespace Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.ViewModel.Signature
 {
     public class SignatureModel
     {
@@ -9,15 +12,18 @@
         public string DataUrl { get; set; }
         public bool HasSigned => !string.IsNullOrWhiteSpace(Data);
         public bool HasProxy => !string.IsNullOrWhiteSpace(Proxy);
+        public DateTime? DoneOn { get; set; }
+        public bool ShowDoneOn { get; set; }
 
-        public static SignatureModel FromDomain(Domain.Inspection.Signature signature)
+        public static SignatureModel FromDomain(Domain.Inspection.Signature signature, FinishStatus finishStatus = null)
         {
             return new SignatureModel
             {
                 Signatory = signature.Signatory,
                 Proxy = signature.Proxy,
                 Data = signature.Data,
-                DataUrl = signature.DataUrl
+                DataUrl = signature.DataUrl,
+                DoneOn = finishStatus?.DoneOn ?? DateTime.Now
             };
         }
     }
