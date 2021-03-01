cb6dcdd0df3545b0bbc8ec2977be89ce5db9056d
Nicolas Destrez <nicolas.destrez@agridea.ch>
2021-03-01 09:19:52 +0100

    switch to loca api. /Login canton dropdown first.

--- Client.Blazor/Settings.cs
+++ Client.Blazor/Settings.cs
@@ -13,7 +13,7 @@
 
         public static Settings Default => new Settings
         {
-            ApiBaseAddres = ProdApiBaseAddres,
+            ApiBaseAddres = LocalhostApiBaseAddres,
             AuthCookieExpiryDays = ThirtyDays
         };
     }
