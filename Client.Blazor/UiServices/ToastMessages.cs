0bf41a6efdabb46feef9ea7f80ee993fa8ed516c
Nicolas Destrez <nicolas.destrez@agridea.ch>
2021-03-09 17:15:22 +0100

    error messages helpers + use in MandateList

--- Client.Blazor/UiServices/ToastMessages.cs
+++ Client.Blazor/UiServices/ToastMessages.cs
@@ -1,4 +1,7 @@
-﻿using Blazored.Toast.Services;
+﻿using System;
+using System.Net.Http;
+using Blazored.Toast.Services;
+using CSharpFunctionalExtensions;
 
 namespace Agridea.Acorda.AcordaControlOffline.Client.Blazor.UiServices
 {
@@ -19,6 +22,39 @@ namespace Agridea.Acorda.AcordaControlOffline.Client.Blazor.UiServices
         public static void Error(this IToastService toast, string message)
         {
             toast.ShowError(message, ToastMessages.ErrorHeader);
+        }
+
+        public static string ToUserErrorMessage(this Exception e)
+        {
+            string errorMessage = "Echec de la connexion au serveur";
+            errorMessage += e switch
+            {
+                HttpRequestException _ => ": serveur inatteignable.",
+                _ => ": erreur inconnue;"
+            };
+            return errorMessage;
+        }
+        public static string ToLogErrorMessage(this Exception e)
+        {
+            string errorMessage = $"Api call failed at {DateTime.Now.ToDetailedTime()}, no response received. " +
+                                  $"Exception details: {e}";
+            return errorMessage;
+        }
+
+        public static string ToLogErrorMessage<T>(this Result<T> apiCallResult)
+        {
+            return $"Failed to fetch {typeof(T).Name} from api. Reason: {apiCallResult.Error}";
+        }
+
+        public static string ToUserErrorMessage<T>(this Result<T> apiCallResult)
+        {
+            string message = "Erreur retournée par le serveur";
+            int httpStatusCodeIndex = apiCallResult.Error.IndexOf("HttpStatusCode =", StringComparison.Ordinal);
+            if (httpStatusCodeIndex <= -1)
+                return message + ", erreur inconnue.";
+
+            string httpStatusCode = apiCallResult.Error.Substring(httpStatusCodeIndex);
+            return message + $", erreur d'api, {httpStatusCode}";
         }
     }
 }
