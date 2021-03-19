fe07dcd172a038da99bd6534f31fcae2e4b1d7fc
Nicolas Destrez <nicolas.destrez@agridea.ch>
2021-03-19 09:52:32 +0100

    Do not serialize calculated properties by default

---
+++ Shared.ApplicationServices/LocalStore/Serialization/ExcludeCalculatedPropertiesContractResolver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.LocalStore.Serialization
+{
+    /// <remarks>
+    /// Thanks to https://stackoverflow.com/a/34112601
+    /// </remarks>
+    public class ExcludeCalculatedPropertiesContractResolver : DefaultContractResolver
+    {
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            var property = base.CreateProperty(member, memberSerialization);
+            property.ShouldSerialize = _ => ShouldSerialize(member);
+            return property;
+        }
+
+        internal static bool ShouldSerialize(MemberInfo memberInfo)
+        {
+            var propertyInfo = memberInfo as PropertyInfo;
+            if (propertyInfo == null)
+            {
+                return false;
+            }
+
+            if (propertyInfo.SetMethod != null)
+            {
+                return true;
+            }
+
+            var getMethod = propertyInfo.GetMethod;
+            return Attribute.GetCustomAttribute(getMethod, typeof(CompilerGeneratedAttribute)) != null;
+        }
+    }
+}
