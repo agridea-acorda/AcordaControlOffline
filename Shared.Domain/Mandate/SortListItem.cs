85a5bcc333e10b4e868c72c4609235c942647584
Tim Allemann <tim.allemann@outlook.com>
2021-07-07 17:58:04 +0200

    Added ordering mandate list, but it is not finished

---
+++ Shared.Domain/Mandate/SortListItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agridea.Acorda.AcordaControlOffline.Shared.Domain.Mandate
+{
+    public class SortListItem
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public bool IsAscending { get; set; }
+    }
+}
