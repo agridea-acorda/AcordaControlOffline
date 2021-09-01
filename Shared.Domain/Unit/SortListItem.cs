146d0f86b4d166c790b8cfe9df24737fac9d9506
Tim Allemann <tim.allemann@outlook.com>
2021-09-01 12:43:55 +0200

    Added new columns unit for testing

---
+++ Shared.Domain/Unit/SortListItem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agridea.Acorda.AcordaControlOffline.Shared.Domain.Unit
+{
+    public class SortListItem
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        // <summary>Gets all data fields to sort</summary>
+        /// <returns>
+        /// The values of the data fields to sort.
+        /// </returns>
+        public static List<SortListItem> GetSortListItems()
+        {
+            List<SortListItem> sortListItemsDatasource = new List<SortListItem>
+            {
+                new SortListItem()
+                {
+                    Id = "Surface (ares)",
+                    Name = "Surface (ares)",
+                },
+                new SortListItem()
+                {
+                    Id = "Longueur (mètre)",
+                    Name = "Longueur (mètre)",
+                },
+                new SortListItem()
+                {
+                    Id = "Nombre (tête, parcelle, analyse ou document manquant/incomplet, etc.)",
+                    Name = "Nombre (tête, parcelle, analyse ou document manquant/incomplet, etc.)",
+                },
+                new SortListItem()
+                {
+                    Id = "Pourcent (%)",
+                    Name = "Pourcent (%)",
+                }
+            };
+            return sortListItemsDatasource;
+        }
+
+        // <summary>Gets the display name of a sortListitem</summary>
+        /// <returns>
+        /// The value of the display name.
+        /// </returns>
+        public static string GetSortListItemDisplayName(SortListItem sortListItem) => sortListItem != null ? $"{sortListItem.Name}" : null;
+    }
+}
