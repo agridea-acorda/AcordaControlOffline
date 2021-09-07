13e65c4bda22508a13ffeb3b4ee1e670cb03a83a
Tim Allemann <tim.allemann@outlook.com>
2021-09-07 16:13:50 +0200

    Added Ktidb, exploitation and domaine filter

---
+++ Shared.Domain/Checklist/Filter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Agridea.Acorda.AcordaControlOffline.Shared.Domain.Mandate;
+
+namespace Agridea.Acorda.AcordaControlOffline.Shared.Domain.Checklist
+{
+    public class Filter
+    {
+        public string Ktidb { get; set; }
+        public string FarmName { get; set; }
+
+        public List<SortListItem> Domains = new List<SortListItem>();
+        // Datasource to populate
+        public List<SortListItem> DomainsDS = new List<SortListItem>();
+
+        public List<SortListItem> Inspectors = new List<SortListItem>();
+        // Datasource to populate
+        public List<SortListItem> InspectorsDS = new List<SortListItem>();
+    }
+}
