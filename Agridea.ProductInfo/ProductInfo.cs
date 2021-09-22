0b0333148a914207ca1949e5261b394f59edd938
Nicolas Destrez <nicolas.destrez@hotmail.com>
2021-09-22 12:48:46 +0200

    Update version

--- Agridea.ProductInfo/ProductInfo.cs
+++ Agridea.ProductInfo/ProductInfo.cs
@@ -18,10 +18,10 @@ namespace Agridea.ProductInfo
     {
         #region This is changed by the build
 
-        public const string Version = "1.3.21244.0"; // major.minor.0.0. Minor is prototype number.
-        public const string FileVersion = "1.3.21244.0"; // major.minor.{julian date}.{number of commits since the most recent tag}
-        public const string InformationalVersion = "1.3 prod g44d1ad7"; // major.minor {branch name} {git commit ID truncated to 8 chars}
-        public const string BuildDate = "2021.09.01 18:45:15";
+        public const string Version = "3.4.21265.0"; // major.minor.0.0. Minor is prototype number.
+        public const string FileVersion = "3.4.21265.0"; // major.minor.{julian date}.{number of commits since the most recent tag}
+        public const string InformationalVersion = "3.4 3 g10af6b0"; // major.minor {branch name} {git commit ID truncated to 8 chars}
+        public const string BuildDate = "2021.09.22 12:45:25";
         public const string Product = "AcordaControlOffline";
 
         #endregion
