82dc2b47d4879c77d322dc5f47662c30a1e9cb17
Tim <tim.allemann@outlook.com>
2022-01-17 13:19:55 +0100

    Added FormInscription

--- Shared.ApplicationServices/Api/IApiClient.cs
+++ Shared.ApplicationServices/Api/IApiClient.cs
@@ -15,6 +15,7 @@ namespace Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.Api {
         Task<Result<ViewModel.Town.Town[]>> FetchAllTownsAsync(string uri);
         Task<Result<Mandate>> FetchMandateDetailAsync(string uri);
         Task<Result<string>> FetchRawJsonAsync(string uri);
+        Task<Result<FormInscription>> FetchFileAsync(string uri);
         Task<Result<ViewModel.Farm.Farm>> FetchFarmDetailAsync(string uri);
         Task<Result<ChecklistSample>> FetchChecklistSampleAsync(string uri);
         Task<Result<MergeResult>> SendMergePackage(string uri, MergePackage mergePackage);
---
+++ Shared.ApplicationServices/IndexedDb/FormInscriptionDb.cs
@@ -0,0 +1,22 @@
+﻿using IndexedDB.Blazor;
+using Microsoft.JSInterop;
+using System;
+
+namespace Agridea.Acorda.AcordaControlOffline.Shared.ApplicationServices.IndexedDb
+{
+    public class FormInscriptionDb : IndexedDB.Blazor.IndexedDb
+    {
+        public FormInscriptionDb(IJSRuntime jSRuntime, string name, int version) : base(jSRuntime, name, version) { }
+        public IndexedSet<FormInscription> FormInscriptions { get; set; }
+    }
+
+    public class FormInscription
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        public int FarmId { get; set; }
+        public byte[] FileData { get; set; }
+        public string FileName { get; set; }
+        public string FileType { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
